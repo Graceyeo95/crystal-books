f8f4b31eda95bd2af2a6f75d0cea3cba102d7470
Crystal <crystalyeo123@hotmail.com>
2025-02-16 23:40:50 +0900

    create Button component with variants and size options

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { twMerge } from 'tailwind-merge';
+import type { VariantProps } from 'class-variance-authority';
+import { cva } from 'class-variance-authority';
 
-const Button = () => {
-  return <div>Button</div>;
+type ButtonProps = {
+  onClick?: () => void;
+  children: React.ReactNode;
+  variant: 'outline' | 'solid' | 'rounded';
+  size: 'small' | 'large';
+  type?: 'submit' | 'reset' | 'button';
+} & VariantProps<typeof buttonVariants>;
+
+const buttonVariants = cva(['rounded-md transition-colors duration-300'], {
+  variants: {
+    variant: {
+      outline:
+        'border-orange bg-transparent border-[1px] text-orange hover:bg-orange hover:text-white',
+      solid:
+        'bg-orange text-white hover:bg-white hover:text-orange border-[1px] border-orange',
+    },
+    size: {
+      small: 'py-2 px-6 text-base',
+      large: 'py-3 px-12 text-lg',
+    },
+  },
+});
+
+const Button = (props: ButtonProps) => {
+  return (
+    <button
+      className={twMerge(
+        buttonVariants(props as VariantProps<typeof buttonVariants>)
+      )}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;

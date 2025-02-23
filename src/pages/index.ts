1863b32b2ec6ef5d90bd79066543b728615db789
Crystal <crystalyeo123@hotmail.com>
2025-02-23 23:41:27 +0900

    add Quotes export to index page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export { default as Home } from './Home';
 export { default as BookDetail } from './BookDetail';
 export { default as SearchFeed } from './SearchFeed';
+export { default as Quotes } from './Quotes';

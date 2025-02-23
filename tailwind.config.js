6d0145f84ac8f9fe73919e62e917ac34917e0814
Crystal <crystalyeo123@hotmail.com>
2025-02-23 21:06:04 +0900

    update Tailwind config: change cream color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ export default {
     extend: {
       colors: {
         orange: '#f04f23',
-        cream: '#fcf8f1',
+        white: '#fcf8f1',
+        cream: '#fdfbd4',
         navy: '#120E23',
       },
     },

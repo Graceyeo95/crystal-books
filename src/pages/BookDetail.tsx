ac82e925f50b344262563e574fed25c07bc30f5a
Crystal <crystalyeo123@hotmail.com>
2025-02-23 23:42:48 +0900

    add toggle functionality for book description in BookDetail component

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -2,12 +2,15 @@ import withTransition from '@/HOC/withTransition';
 import { useParams } from 'react-router-dom';
 import { FallingLines } from 'react-loader-spinner';
 import { useBookDetail } from '@/hooks/useFetch';
+import { truncateText } from '@/utils/helpers';
+import { useState } from 'react';
 
 type GenreType = {
   name: string;
 };
 
 const BookDetail = () => {
+  const [isShowMore, setIsShowMore] = useState(false);
   const { id } = useParams();
   const { data, isLoading, isError, error } = useBookDetail(id!);
 
@@ -21,9 +24,13 @@ const BookDetail = () => {
 
   if (isError) return <div>Error: {error?.message}</div>;
 
+  const truncatedDescription = (text: string) => {
+    return isShowMore ? text : truncateText(text, 500);
+  };
+
   return (
-    <div className='text-white text-3xl text-center'>
-      <div className='h-[120px] md:h-[160px] lg:h-[200px]' />
+    <div className='text-white text-3xl text-center bg-orange'>
+      <div className='h-[160px] lg:h-[200px]' />
       <div className='max-w-[80vw] gap-y-8 lg:gap-y-12 flex flex-col lg:flex-row mx-auto justify-center gap-x-20 mb-20'>
         <div className='w-full lg:w-1/2 max-w-[240px] md:max-w-[300px] lg:max-w-[400px] mx-auto lg:mx-0'>
           <img
@@ -48,7 +55,17 @@ const BookDetail = () => {
               </span>
             ))}
           </div>
-          <p className='smallBodyText'>{data?.description}</p>
+          <div className='smallBodyText'>
+            <div>
+              {truncatedDescription(data.description)}{' '}
+              <button
+                onClick={() => setIsShowMore((prev) => !prev)}
+                className='underline'
+              >
+                Read {isShowMore ? 'Less' : 'More'}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 

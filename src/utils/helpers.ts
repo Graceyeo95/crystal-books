c1c1eda78633ef848af4e2ab01947df10fe60bc1
Crystal <crystalyeo123@hotmail.com>
2025-02-23 23:39:38 +0900

    enhance resizeImageUrl function to handle empty URLs and improve regex checks

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,9 +1,17 @@
 export const resizeImageUrl = (url: string) => {
-  const regexX = /(_SX\d+_)/;
-  const regexY = /(_SY\d+_)/;
-  return regexX
-    ? url.replace(regexX, `_SX300_`)
-    : url.replace(regexY, `_SY300_`);
+  if (!url) return '';
+
+  const regexX = /_SX\d+_/;
+  const regexY = /_SY\d+_/;
+
+  if (regexX.test(url)) {
+    return url.replace(regexX, '_SX300_');
+  }
+  if (regexY.test(url)) {
+    return url.replace(regexY, '_SY300_');
+  }
+
+  return url;
 };
 
 export const truncateText = (text: string, length: number) => {

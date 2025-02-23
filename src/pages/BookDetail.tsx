c3bc82d4b306a199f2ec790118194961a5348fe2
Crystal <crystalyeo123@hotmail.com>
2025-02-23 21:07:03 +0900

    refactor BookDetail component: enhance layout and styling, add genre display

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -2,13 +2,15 @@ import withTransition from '@/HOC/withTransition';
 import { useParams } from 'react-router-dom';
 import { FallingLines } from 'react-loader-spinner';
 import { useBookDetail } from '@/hooks/useFetch';
-import { resizeImageUrl } from '@/utils/helpers';
+
+type GenreType = {
+  name: string;
+};
 
 const BookDetail = () => {
-  const { id } = useParams(); // Get bookId from URL
+  const { id } = useParams();
   const { data, isLoading, isError, error } = useBookDetail(id!);
 
-  // If loading, show the spinner
   if (isLoading) {
     return (
       <div className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
@@ -17,26 +19,40 @@ const BookDetail = () => {
     );
   }
 
-  // If there's an error, show error message
   if (isError) return <div>Error: {error?.message}</div>;
 
   return (
     <div className='text-white text-3xl text-center'>
-      <div className='h-[100px]' />
-      <div className='border-2 max-w-[80vw] mx-auto flex justify-center gap-x-32'>
-        <div>
+      <div className='h-[120px] md:h-[160px] lg:h-[200px]' />
+      <div className='max-w-[80vw] gap-y-8 lg:gap-y-12 flex flex-col lg:flex-row mx-auto justify-center gap-x-20 mb-20'>
+        <div className='w-full lg:w-1/2 max-w-[240px] md:max-w-[300px] lg:max-w-[400px] mx-auto lg:mx-0'>
           <img
-            src={resizeImageUrl(data.imageUrl)}
+            src={data.imageUrl}
             alt={data.title}
-            className='w-[300px] aspect-[3/4]'
+            className='w-full aspect-[3/4]'
           />
         </div>
 
-        <div>
-          <h1>{data?.title}</h1>
-          <p>{data?.description}</p>
+        <div className='my-auto space-y-3 text-center lg:text-left w-full lg:w-1/2'>
+          <h1 className='largeBodyText font-bold'>{data?.title}</h1>
+          <p className='smallBodyText text-neutral-200'>
+            By {data?.author.name}
+          </p>
+          <div className='flex flex-wrap gap-2 justify-center lg:justify-start'>
+            {data?.bookGenres.map((genre: GenreType) => (
+              <span
+                key={genre.name}
+                className='text-xs border-[2px] py-1 px-2 rounded-lg'
+              >
+                {genre.name}
+              </span>
+            ))}
+          </div>
+          <p className='smallBodyText'>{data?.description}</p>
         </div>
       </div>
+
+      <div className='bg-cream h-[500px]'>testing</div>
     </div>
   );
 };

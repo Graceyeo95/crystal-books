9b496159023de5b2257ba1004add4a02af3d7635
Crystal <crystalyeo123@hotmail.com>
2025-02-24 22:26:52 +0900

    feat: update transition easing for improved animation in withTransition HOC

--- src/HOC/withTransition.tsx
+++ src/HOC/withTransition.tsx
@@ -11,14 +11,14 @@ const widthTransition = (Component: React.FC<{}>) => {
           initial={{ scaleY: 0 }}
           animate={{ scaleY: 0 }}
           exit={{ scaleY: 1 }}
-          transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+          transition={{ duration: 0.8, ease: [0.43, 0.13, 0.23, 0.9] }}
         />
         <motion.div
           className='slide-out'
           initial={{ scaleY: 1 }}
           animate={{ scaleY: 0 }}
           exit={{ scaleY: 0 }}
-          transition={{ duration: 0.8, ease: [0.22, 1, 0.36, 1] }}
+          transition={{ duration: 0.8, ease: [0.43, 0.13, 0.23, 0.9] }}
         />
       </>
     );

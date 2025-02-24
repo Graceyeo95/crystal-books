7a56a324b74d0fa8a0ffcc000f7de108e3d16c2e
Crystal <crystalyeo123@hotmail.com>
2025-02-24 22:06:36 +0900

    feat: add favorite toggle function and back navigation in BookDetail component

--- src/pages/BookDetail.tsx
+++ src/pages/BookDetail.tsx
@@ -1,5 +1,4 @@
-import withTransition from '@/HOC/withTransition';
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { FallingLines } from 'react-loader-spinner';
 import { useBookDetail } from '@/hooks/useFetch';
 import { truncateText } from '@/utils/helpers';
@@ -7,6 +6,10 @@ import { useState } from 'react';
 import Button from '../components/Button';
 import { motion } from 'framer-motion';
 import { slideIn } from '@/utils/animation';
+import { MdFavorite, MdFavoriteBorder, MdArrowBackIos } from 'react-icons/md';
+import { useFavoritesStore } from '@/store/useFavoritesStore';
+import withTransition from '@/HOC/withTransition';
+import { toast, ToastContainer } from 'react-toastify';
 
 type GenreType = {
   name: string;
@@ -15,7 +18,9 @@ type GenreType = {
 const BookDetail = () => {
   const [isShowMore, setIsShowMore] = useState(false);
   const { id } = useParams();
-  const { data, isLoading, isError } = useBookDetail(id!);
+  const { data, isLoading, isError, error } = useBookDetail(id!);
+  const { addFavorite, removeFavorite, isFavorite } = useFavoritesStore();
+  const navigate = useNavigate();
 
   if (isLoading)
     return (
@@ -24,10 +29,11 @@ const BookDetail = () => {
       </div>
     );
 
-  if (isError)
+  if (isError) {
+    console.error('Error fetching book details:', error);
     return (
       <div className='w-screen h-screen bg-orange'>
-        <div className='max-w-[80vw] mx-auto flex flex-col items-center justify-center gap-y-8 text-center text-white text-xl lg:text-2xl'>
+        <div className='w-full h-full max-w-[80vw] mx-auto flex flex-col items-center justify-center gap-y-8 text-center text-white text-xl lg:text-2xl'>
           Sorry, there was a problem while fetching books
           <Link to='/'>
             <Button
@@ -41,61 +47,107 @@ const BookDetail = () => {
         </div>
       </div>
     );
+  }
+
+  const handleFavoriteToggle = () => {
+    if (isFavorite(data.id, 'book')) {
+      removeFavorite(data.id, 'book');
+      toast.success('Removed from favourites');
+    } else {
+      addFavorite(
+        {
+          id: data.id,
+          imageUrl: data.imageUrl,
+          title: data.title,
+          description: data.description,
+        },
+        'book'
+      );
+      toast.success('Added to favourites');
+    }
+  };
 
   const truncatedDescription = (text: string) => {
-    if (!text) return;
+    if (!text) return '';
     return isShowMore ? text : truncateText(text, 500);
+  };
+
+  const handleBackClick = () => {
+    navigate(-1);
   };
 
   return (
     <div className='text-white text-3xl text-center bg-navy'>
+      <ToastContainer
+        style={{ fontSize: '16px', color: 'black' }}
+        autoClose={3000}
+        pauseOnFocusLoss={false}
+      />
       <div className='h-[160px]' />
-      <div className='max-w-[80vw] gap-y-8 lg:gap-y-12 container-wrapper flex flex-col lg:flex-row mx-auto justify-center gap-x-20'>
-        <motion.div
-          variants={slideIn('left', 1, 0.2)}
-          initial='hidden'
-          whileInView='show'
-          viewport={{ once: false, amount: 0.9 }}
-          className='w-full lg:w-1/2 max-w-[240px] md:max-w-[300px] lg:max-w-[400px] mx-auto lg:mx-0'
+      <div className='flex flex-col max-w-[80vw] mx-auto'>
+        <button
+          onClick={handleBackClick}
+          className='text-base mr-auto flex w-[80px]'
         >
-          <img
-            src={data.imageUrl}
-            alt={data.title}
-            className='w-full aspect-[3/4]'
-          />
-        </motion.div>
+          <MdArrowBackIos size={24} /> Back
+        </button>
+        <div className='gap-y-8 lg:gap-y-12 container-wrapper flex flex-col lg:flex-row mx-auto justify-center gap-x-20'>
+          <motion.div
+            variants={slideIn('left', 1, 0.2)}
+            initial='hidden'
+            whileInView='show'
+            className='w-full lg:w-1/2 max-w-[240px] md:max-w-[300px] lg:max-w-[400px] mx-auto lg:mx-0'
+          >
+            {data?.imageUrl && (
+              <img
+                src={data.imageUrl}
+                alt={data.title}
+                className='w-full aspect-[3/4]'
+              />
+            )}
+          </motion.div>
 
-        <motion.div
-          variants={slideIn('right', 1, 0.2)}
-          initial='hidden'
-          whileInView='show'
-          viewport={{ once: false }}
-          className='my-auto space-y-3 text-center lg:text-left w-full lg:w-1/2'
-        >
-          <h1 className='largeBodyText font-bold'>{data?.title}</h1>
-          <p className='smallBodyText text-neutral-200'>
-            By {data?.author.name}
-          </p>
-          <div className='flex flex-wrap gap-2 justify-center lg:justify-start'>
-            {data?.bookGenres.map((genre: GenreType) => (
-              <span
-                key={genre.name}
-                className='text-xs border-[2px] py-1 px-2 rounded-lg'
-              >
-                {genre.name}
-              </span>
-            ))}
-          </div>
-          <div className='smallBodyText'>
-            {truncatedDescription(data.description)}{' '}
+          <motion.div
+            variants={slideIn('right', 1, 0.2)}
+            initial='hidden'
+            whileInView='show'
+            className='my-auto space-y-3 text-center lg:text-left w-full lg:w-1/2'
+          >
             <button
-              onClick={() => setIsShowMore((prev) => !prev)}
-              className='underline'
+              onClick={handleFavoriteToggle}
+              className='text-base flex items-center gap-x-2 mx-auto lg:mx-0 text-orange'
             >
-              Read {isShowMore ? 'Less' : 'More'}
+              {isFavorite(data.id, 'book') ? (
+                <MdFavorite size={24} />
+              ) : (
+                <MdFavoriteBorder size={24} />
+              )}
             </button>
-          </div>
-        </motion.div>
+            <h1 className='largeBodyText font-bold'>{data?.title}</h1>
+            <p className='smallBodyText text-neutral-200'>
+              By {data?.author?.name}
+            </p>
+            <div className='flex flex-wrap gap-2 justify-center lg:justify-start'>
+              {data?.bookGenres?.map((genre: GenreType) => (
+                <span
+                  key={genre.name}
+                  className='text-xs border-[2px] py-1 px-2 rounded-lg'
+                >
+                  {genre.name}
+                </span>
+              ))}
+            </div>
+            <div className='smallBodyText'>
+              {truncatedDescription(data.description)}{' '}
+              <button
+                onClick={() => setIsShowMore((prev) => !prev)}
+                className='underline'
+              >
+                Read {isShowMore ? 'Less' : 'More'}
+              </button>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );

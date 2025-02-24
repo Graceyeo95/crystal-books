8fae7d29ba7973e3c937b990af13619da6e628b5
Crystal <crystalyeo123@hotmail.com>
2025-02-24 21:50:51 +0900

    style: update text color in AuthorSuggestBanner component

--- src/components/AuthorSuggestBanner.tsx
+++ src/components/AuthorSuggestBanner.tsx
@@ -9,11 +9,10 @@ const AuthorSuggestBanner = () => {
     <div className='w-full z-10 text-white bg-navy py-32'>
       <div className='grid grid-cols-1 lg:grid-cols-6 mx-auto max-w-[85vw] lg:max-w-[90vw]'>
         <div className='col-span-2 m-auto text-center lg:text-left flex flex-col'>
-          <p className='subHeadingText font-bold'>
-            베스트셀러 작가 한강의
-            <br />꼭 읽어야 할 작품들!
+          <p className='subHeadingText font-bold text-pretty'>
+            베스트셀러 작가 한강의 꼭 읽어야 할 작품들!
           </p>
-          <p className='smallBodyText mt-2'>
+          <p className='smallBodyText mt-2 text-pretty'>
             문학계를 사로잡은 한강의 대표작을 만나보세요. 깊이 있는 스토리와
             감동을 경험할 시간!
           </p>

30505616f0eefc33539c2c39cf6607306f8fcab7
Crystal <crystalyeo123@hotmail.com>
2025-02-24 22:02:58 +0900

    refactor: change data fetching logic into reusable useFetchData hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -6,46 +6,35 @@ import {
   getAuthorsBooks,
 } from '../services/api';
 
-export const useSearchBooks = (query: string) => {
+const useFetchData = <T>(
+  queryKey: (string | number)[],
+  fetchFn: () => Promise<T>,
+  enabled: boolean
+) => {
   const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['searchBooks', query],
-    queryFn: () => searchBooks(query),
-    enabled: !!query,
-    staleTime: 600000,
-  });
-
-  return { data, isLoading, isError, error };
-};
-
-export const useSearchQuotes = (query: string) => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['searchQuotes', query],
-    queryFn: () => searchQuotes(query),
-    enabled: !!query,
-    staleTime: 60000,
-  });
-
-  return { data, isLoading, isError, error };
-};
-
-export const useBookDetail = (bookId: string) => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['bookDetail', bookId],
-    queryFn: () => getBookById(bookId),
-    enabled: !!bookId,
+    queryKey,
+    queryFn: fetchFn,
+    enabled,
     staleTime: Infinity,
+    retry: 1,
+    refetchOnWindowFocus: false,
   });
 
   return { data, isLoading, isError, error };
 };
 
-export const useAuthorsBooks = (authorId: string) => {
-  const { data, isLoading, isError, error } = useQuery({
-    queryKey: ['bookDetail', authorId],
-    queryFn: () => getAuthorsBooks(authorId),
-    enabled: !!authorId,
-    staleTime: 60000,
-  });
+export const useSearchBooks = (query: string) =>
+  useFetchData(['searchBooks', query], () => searchBooks(query), !!query);
 
-  return { data, isLoading, isError, error };
-};
+export const useSearchQuotes = (query: string) =>
+  useFetchData(['searchQuotes', query], () => searchQuotes(query), !!query);
+
+export const useBookDetail = (bookId: string) =>
+  useFetchData(['bookDetail', bookId], () => getBookById(bookId), !!bookId);
+
+export const useAuthorsBooks = (authorId: string) =>
+  useFetchData(
+    ['authorsBooks', authorId],
+    () => getAuthorsBooks(authorId),
+    !!authorId
+  );

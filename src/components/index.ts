566a5ab54860304f9371349754b9be3493ec2dad
Crystal <crystalyeo123@hotmail.com>
2025-02-24 22:00:38 +0900

    feat: add QuoteCard component to index exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Navbar } from './Navbar';
 export { default as BookCard } from './BookCard';
+export { default as QuoteCard } from './QuoteCard';
 export { default as HeroBanner } from './HeroBanner';
 export { default as HighlightBlock } from './HighlightBlock';
 export { default as TransparentBanner } from './TransparentBanner';

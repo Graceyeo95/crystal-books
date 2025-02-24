8d951425233f23cfb85ca82a159b602a42ac0171
Crystal <crystalyeo123@hotmail.com>
2025-02-24 21:50:32 +0900

    Add Toast and NotFound page in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Navbar, Footer } from '@/components';
 import { AnimatePresence } from 'framer-motion';
-import { Home, BookDetail, SearchFeed, Quotes, Favourites } from '@/pages';
+import {
+  Home,
+  BookDetail,
+  SearchFeed,
+  Quotes,
+  Favourites,
+  NotFound,
+} from '@/pages';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 const queryClient = new QueryClient();
 
@@ -10,6 +19,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
+        <ToastContainer
+          style={{ fontSize: '16px', color: 'black' }}
+          autoClose={3000}
+          pauseOnFocusLoss={false}
+        />
         <Navbar />
         <AnimatedRoutes />
         <Footer />
@@ -29,6 +43,7 @@ const AnimatedRoutes = () => {
         <Route path='/favourite' element={<Favourites />} />
         <Route path='/book/:id' element={<BookDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        <Route path='/*' element={<NotFound />} />
       </Routes>
     </AnimatePresence>
   );
